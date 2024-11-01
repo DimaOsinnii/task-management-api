715fbbd1ce23a190b86d63adcacc2f878cf7c416
autumn_dima <ozerland@gmail.com>
2024-11-01 15:57:21 +0200

    add husky add lint-staged

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,11 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
+import prettier from 'eslint-config-prettier';
 
 export default [
   {
     languageOptions: { globals: globals.node },
   },
   pluginJs.configs.recommended,
+  prettier,
 ];
